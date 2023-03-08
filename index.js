6d74df417230e48e443a9a1d893c365bb5c39e35
Aymanoli <aymanoli777@gmail.com>
2023-03-08 22:45:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { MongoClient} = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.far0qag.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('database connect successfully')
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) =>{
+    res.send('Ema jon server is runnig');
+});
+
+app.listen(port, () =>{
+    console.log('server running at port', port);
+});
